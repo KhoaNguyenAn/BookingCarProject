60fe6afa2c3a70dc3478c2adfbf844553f39c89c
Vedesh Appadoo <vapp0002@student.monash.edu>
2021-05-11 11:03:14 +0400

    Map

---
+++ A2/app/js/map.js
@@ -0,0 +1,11 @@
+mapboxgl.accessToken = "pk.eyJ1IjoidmVkZXNoYXBwYWRvbyIsImEiOiJja28yeXd4NXcwM3U3MndteG85ZmRyOXlqIn0.2-JvhfDcqdwAd1ezlYJBVQ";
+let map = new  mapboxgl.Map({
+	 container: 'map',
+	center: [144.9648731,-37.8182711],
+	zoom: 16,
+	style: 'mapbox://styles/mapbox/streets-v9'
+});
+function panToClayton(){}
+function showPath(){}
+function showPolygon(){}
+function showCircle(){}
